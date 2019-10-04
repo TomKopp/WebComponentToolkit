f205e693a3b7486bc2d9d5de455a30e094aba0c7
Kopp <witzbould@outlook.com>
2019-10-04 20:03:07 +0200

    export Card Accordion AccordionTab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 export * from './components/Base/base-element';
 export * from './decorators';
 export * from './utility';
+export * from './components/Accordion/accordion-tab';
+export * from './components/Accordion/accordion';
+export * from './components/Card/card';
