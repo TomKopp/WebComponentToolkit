0ed4ec5472c1c895ead9709eac0987c80ca0f8c2
Kopp <witzbould@outlook.com>
2019-10-15 16:36:36 +0200

    update AccordionTab with change handler, event dispatcher, markup and styling

--- src/components/Accordion/accordion-tab.js
+++ src/components/Accordion/accordion-tab.js
@@ -3,45 +3,77 @@ import { BaseElement } from '../Base/base-element';
 
 @defineElement('wctk-accordion-tab')
 export class AccordionTab extends BaseElement {
+	ActivationEvent = new CustomEvent('AccordionTab', {
+		bubbles: true
+		, composed: true
+		, detail: this
+	});
+
 	@property()
 	header = 'Accordion tab default header';
 
+	@property({
+		observe: false
+		, onChange() {
+			this.classList.toggle('active');
+			this.dispatchEvent(this.ActivationEvent);
+		}
+	})
+	active = false;
 
 	updateTemplate() {
-		return `<section class="accordion-tab">
-	<div class="accordion-tab-header">
-		<i class="accordion-tab-header-ico"><svg width="1em" height="1em" viewBox="0 0 24 24" style="fill: currentcolor;"><path d="M8.59 16.34l4.58-4.59-4.58-4.59L10 5.75l6 6-6 6z"></path></svg></i>
+		return `<div class="header">
+		<i class="header-ico"><svg width="1em" height="1em" viewBox="0 0 24 24" style="fill: currentcolor;"><path d="M8.59 16.34l4.58-4.59-4.58-4.59L10 5.75l6 6-6 6z"></path></svg></i>
 		<slot name="header">${this.header}</slot>
 	</div>
-	<div class="accordion-tab-content"><slot></slot></div>
-</section>`;
+	<div class="collapsable"><div class="content"><slot></slot></div></div>`;
 	}
 
 	updateStyle() {
 		return `:host {
 	display: block;
 }
-.accordion-tab-header {
+:host([hidden]) {
+	display: none;
+}
+.header {
 	align-items: center;
 	background-color: #fff;
 	cursor: pointer;
 	display: flex;
 	padding: 1rem;
-	transition: background-color 0.3s;
+	transition: background-color 0.2s;
 }
-.accordion-tab-header-ico {
-	font-size: 1.125rem;
+.header-ico > svg {
+	height: 1.125rem;
 	margin-right: 1rem;
+	transform-origin: center;
 	transition: transform 0.2s;
+	width: 1.125rem;
 }
-.accordion-tab-content {
-	padding: 0.5rem 1rem 2rem 3.125rem;
+.collapsable {
+	max-height: 0;
+	overflow: hidden;
+	transition: max-height 0.2s;
 }
-:host(.active) .accordion-tab-header {
+.content {
+	margin: 0.5rem 1rem 2rem 3.125rem;
+}
+:host(.active) .header {
 	background-color: rgb(248, 248, 248);
 }
-:host(.active) .accordion-tab-header-ico {
+:host(.active) .header-ico > svg {
 	transform: rotate(90deg);
+}
+:host(.active) .collapsable {
+	max-height: 100vh;
 }`;
+	}
+
+	preRender() {
+		const header = this.template.content.querySelector('.header');
+		if (header) {
+			header.addEventListener('click', () => (this.active = !this.active));
+		}
 	}
 }
