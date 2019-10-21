00cac33605b34e18819201f85c00e8543e48968a
Kopp <witzbould@outlook.com>
2019-10-21 18:40:04 +0200

    update state store

--- src/state-store.js
+++ src/state-store.js
@@ -1,30 +1,56 @@
 // State store for e.g. class properties
 
 const store = Symbol('store');
+const observers = Symbol('observers');
+
+class Entity {
+	constructor(key, value, newValue) {
+		this.key = key;
+		this.value = value;
+		this.newValue = newValue;
+	}
+}
 
 export const StateStore = Object.seal(class StateStore {
 	constructor() {
 		this[store] = new Map();
+		this[observers] = [];
+		this.mutationEvent = new Event('mutation', { cancelable: true });
 	}
 
 	get(key) {
 		return this[store].get(key);
 	}
 
-	set(key, value) {
-		const oldVal = this[store].get(key);
-		this[store].set(key, value);
+	set(key, newValue) {
+		this.mutation(new Entity(key, this[store].get(key), newValue))
+			.then(this.saveToStore.bind(this))
+			.catch((err) => err)
+			.finally(this.notify.bind(this));
 
-		return this.mutation(oldVal, this[store].get(key));
+		return this;
 	}
 
-	mutation(oldVal, newVal) {
+	saveToStore(entity) {
+		this[store].set(entity.key, entity.newValue);
+		return entity;
+	}
+
+	notify() {
+		this[observers].reduce((promiseAcc, func) => promiseAcc.then(func), Promise.resolve());
+	}
+
+	mutation(entity) {
 		return new Promise((resolve, reject) => {
-			if (this.dispatchEvent(new Event('mutation', { cancelable: true }))) {
-				resolve({ oldVal, newVal });
+			if (this.dispatchEvent(this.mutationEvent)) {
+				resolve(entity);
 			}
 
 			reject(new Error('Mutation event was canceled'));
 		});
+	}
+
+	observe(callback) {
+		this[observers].push(callback);
 	}
 });
